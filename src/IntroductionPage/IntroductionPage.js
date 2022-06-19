963509bbc347183dc9c5b006d5f4874a4fbe2e00
Nandhakumar <nandhakumarmk98@gmail.com>
2022-06-20 02:29:32 +0530

    Updated introductionPage with css

--- src/IntroductionPage/IntroductionPage.js
+++ src/IntroductionPage/IntroductionPage.js
@@ -1,7 +1,16 @@
 import React from "react";
 import logo from "../resources/images/logo.png";
+import "./IntroductionPage.css";
+
 const IntroductionPage = () => {
-  return <div>Welcome to introduction page</div>;
+  return (
+    <div className="introduction_page_container">
+      <div className="introduction_page_panel">
+        <img className="introduction_page_image" src={logo} alt="logo"></img>
+        buttons
+      </div>
+    </div>
+  );
 };
 
 export default IntroductionPage;
