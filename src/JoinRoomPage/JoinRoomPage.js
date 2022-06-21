fde09f2005ea56240cf508d25441c9c93301be23
Nandhakumar <nandhakumarmk98@gmail.com>
2022-06-21 23:59:01 +0530

    Added container for join room page

--- src/JoinRoomPage/JoinRoomPage.js
+++ src/JoinRoomPage/JoinRoomPage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import "./JoinRoomPage.css";
 
-const JoinRoomPage = () => {
-  return <div>Welcome to join page</div>;
+const JoinRoomPage = (props) => {
+  const search = useLocation().search;
+
+  useEffect(() => {
+    const isRoomHost = new URLSearchParams(search).get("host");
+    if (isRoomHost) {
+      //setting host as true in redux
+    }
+  }, [search]);
+
+  return (
+    <div className="join_room_page_container">
+      <div className="join_room_page_panel"></div>
+    </div>
+  );
 };
 
 export default JoinRoomPage;
